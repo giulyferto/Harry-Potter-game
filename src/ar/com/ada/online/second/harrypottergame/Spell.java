01db2b1d0cea3ff44324f5f2f0526de68bceb4d1
Giuliana Fertonani <giulyferto@gmail.com>
2020-11-01 21:33:14 -0300

    Se corrige clase Spell

--- src/ar/com/ada/online/second/harrypottergame/Spell.java
+++ src/ar/com/ada/online/second/harrypottergame/Spell.java
@@ -1,61 +1,61 @@
 package ar.com.ada.online.second.harrypottergame;
 
 public class Spell {
-    public class Spell {
-        private String name;
-        private Integer damage;
-        private Integer recovery;
-        private Integer magicPower;
-        private Integer counter = 0;
+    private String name;
+    private Integer damage;
+    private Integer recovery;
+    private Integer magicPower;
+    private Integer counter = 0;
 
 
-        public Spell() {
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Integer getDamage() {
-            return damage;
-        }
-
-        public void setDamage(Integer damage) {
-            this.damage = damage;
-        }
-
-        public Integer getRecovery() {
-            return recovery;
-        }
-
-        public void setRecovery(Integer recovery) {
-            this.recovery = recovery;
-        }
-
-        public Integer getMagicPower() {
-            return magicPower;
-        }
-
-        public void setMagicPower(Integer magicPower) {
-            this.magicPower = magicPower;
-        }
-
-        public Integer getCounter() {
-            return counter;
-        }
-
-        public void setCounter(Integer counter) {
-            this.counter = counter;
-        }
-        //Metodo para contar el tipo de hechizos de ataque
-        public Integer addAttackSpell() {
-            counter++;
-            return counter;
-        }
-
+    public Spell() {
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDamage() {
+        return damage;
+    }
+
+    public void setDamage(Integer damage) {
+        this.damage = damage;
+    }
+
+    public Integer getRecovery() {
+        return recovery;
+    }
+
+    public void setRecovery(Integer recovery) {
+        this.recovery = recovery;
+    }
+
+    public Integer getMagicPower() {
+        return magicPower;
+    }
+
+    public void setMagicPower(Integer magicPower) {
+        this.magicPower = magicPower;
+    }
+
+    public Integer getCounter() {
+        return counter;
+    }
+
+    public void setCounter(Integer counter) {
+        this.counter = counter;
+    }
+
+    //Metodo para contar el tipo de hechizos de ataque
+    public Integer addAttackSpell() {
+        counter++;
+        return counter;
+    }
+
 }
+
